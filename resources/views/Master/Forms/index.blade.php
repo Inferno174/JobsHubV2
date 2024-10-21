52bc450af357df44aac99b9ec419b529826f0b41
Melvin <melvinrosario8248@gmail.com>
2024-10-21 18:59:25 +0530

    21-10

--- resources/views/Master/Forms/index.blade.php
+++ resources/views/Master/Forms/index.blade.php
@@ -1,9 +1,60 @@
 @extends('layouts.adminlayout')
 @section('content')
 <!-- Button trigger modal -->
-<div class="d-flex justify-content-end">
-    <button type="button" class="btn btn-primary mb-2" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">New Form <i class="fa-solid fa-plus"></i></button>
-</div>
+<div class="d-flex justify-content-end align-items-center mt-0">
+    <div>
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModalCenter">
+            Add <i class="fa-solid fa-plus"></i>
+         </button>
+    </div>
+    <div class="accordion accordion-flush" id="accordionFlushExample">
+       <div class="accordion-item mx-2">
+          <button class="collapsed btn btn-primary" id="filter-button" style="margin-top: 20px" type="button" data-bs-toggle="collapse"
+                  data-bs-target="#flush-collapseOne" aria-expanded="false"
+                  aria-controls="flush-collapseOne">
+             <i class="fa-solid fa-filter filter"></i>
+          </button>
+       </div>
+    </div>
+    <div class="basic-dropdown mx-1">
+       <div class="dropdown">
+          <button type="button" class="btn btn-primary dropdown-toggle" data-bs-toggle="dropdown">
+             Export
+          </button>
+          <div class="dropdown-menu">
+             <a class="dropdown-item" href="#">Excel</a>
+          </div>
+       </div>
+    </div>
+ </div>
+
+ {{-- filter form --}}
+ <form action="" id="filter-form">
+    @csrf
+    <div id="flush-collapseOne" class="accordion-collapse collapse" data-bs-parent="#accordionFlushExample" style="display: none;">
+        <div class="accordion-body row d-flex justify-content-between align-items-center">
+            <div class="col-lg-6">
+                <div class="basic-form">
+                    <div class="form-group mb-2">
+                        <label>Job Category</label>
+                        <select name="job_category" class="form-control default-select">
+                            <option value="">Select Job Category</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <div class="form-group">
+                    <label>Job Name</label>
+                    <input type="text" name="job_name" class="form-control" placeholder="Enter Details" value="">
+                </div>
+            </div>
+        </div>
+        <div style="display: flex; justify-content: flex-end; align-items: center;">
+            <button type="button" class="btn btn-primary mb-2">Apply Filter</button>
+        </div>
+    </div>
+</form>
 <!-- Modal -->
 <div class="modal fade" id="exampleModalCenter">
     <div class="modal-dialog modal-dialog-centered" role="document">
@@ -72,9 +123,6 @@
 <script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
 <script src="https://cdn.datatables.net/1.13.7/js/dataTables.bootstrap5.min.js"></script>
 
-<!-- Bootstrap Bundle -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-
 <!-- Your own DataTable initialization -->
 <script>
     $(document).ready(function(){
@@ -89,6 +137,18 @@
         });
     });
 </script>
+<script>
+    document.addEventListener('DOMContentLoaded', function() {
+    const filterButton = document.getElementById('filter-button');
+    const accordionBody = document.getElementById('flush-collapseOne');
 
-
+    filterButton.addEventListener('click', function() {
+        if (accordionBody.style.display === 'none' || accordionBody.style.display === '') {
+            accordionBody.style.display = 'block'; // Show the accordion body
+        } else {
+            accordionBody.style.display = 'none';  // Hide the accordion body
+        }
+    });
+});
+    </script>
 @endsection
